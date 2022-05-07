5aa8faed795a88a9ccff9d066592813589e78249
Jannatul Ferdous <jannatpinky92@gmail.com>
2022-05-07 15:54:35 -0400

    adding delete button

--- src/component/ManageInventory/ManageInventory.js
+++ src/component/ManageInventory/ManageInventory.js
@@ -2,13 +2,31 @@ import React, { useEffect, useState } from 'react';
 
 const ManageInventory = () => {
 
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data))
 
-    }, [])
+    }, []);
+    const handleUserDelete = id =>{
+        const proceed = window.confirm('Are you sure you want to delete?');
+        if(proceed){
+            console.log('deleting user with id, ', id);
+            const url = `http://localhost:5000/product/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data =>{
+                if(data.deletedCount > 0){
+                    console.log('deleted');
+                    const remaining = products.filter(product=> product._id !== id);
+                    setProducts(remaining);
+                }
+            })
+        }
+    }
     return (
         <div>
             <h1>mange inventory</h1>
@@ -16,6 +34,12 @@ const ManageInventory = () => {
             <div >
             {
             products.map(product =><div key={product._id}>
+                 <img style={{width:'200px'}} src={product.picture} alt="" />
+                <h3>{product.name}</h3>
+                <h5>{product.supplierName}</h5>
+                <p>{product.price}</p>
+                <p>{product.description}</p>
+                <button onClick={() => handleUserDelete(product._id)} >Delete</button>
                
             </div>)
         }
